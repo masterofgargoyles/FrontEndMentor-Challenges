ac92519171f7beb7496eb4ba930049c27f38bdfb
Yudhajit Chowdhury <110385709+masterofgargoyles@users.noreply.github.com>
2023-03-06 16:28:34 +0530

    1️⃣

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -1,8 +1,10 @@
 const mainContainer = document.querySelector(".main-container")
-const thankYouContainer = document.querySelecter(".thank-you")
+const thankYouContainer = document.querySelector(".thank-you")
 const submitButton = document.getElementById("submit")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn")
 
 submitButton.addEventListener("click",()=>{
     thankYouContainer.classList.remove("hidden")
-    mainContainer.style.display="none"
+    mainContainer.classList.add("hidden")
 })
